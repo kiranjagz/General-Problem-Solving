e3296aa85e839e80d97d8f5f9e615c25317ff49e
Kiran <kiranjagz@gmai.com>
2022-07-22 14:38:37 +0200

    refactor: uncommented code to run.

--- General-Problem-Solving/Program.cs
+++ General-Problem-Solving/Program.cs
@@ -9,23 +9,23 @@ namespace General_Problem_Solving
     {
         static void Main(string[] args)
         {
-            //var mulitples = new Multiples();
-            //var reverse = new ReverseSentence();
-            //var findPrimeCount = new FindPrimeNumbers();
-            //var findEvenSum = new FibonacciNumbers();
+            var mulitples = new Multiples();
+            var reverse = new ReverseSentence();
+            var findPrimeCount = new FindPrimeNumbers();
+            var findEvenSum = new FibonacciNumbers();
 
-            //var multipleResult = mulitples.SumOfThreeAndFiveMultiples(1000);
+            var multipleResult = mulitples.SumOfThreeAndFiveMultiples(1000);
 
-            //reverse.LongReverse("The Rain in Spain");
+            reverse.LongReverse("The Rain in Spain");
 
-            //var primeCount = findPrimeCount.FindCountOfPrimeNumber();
+            var primeCount = findPrimeCount.FindCountOfPrimeNumber();
 
-            //var evenSum = findEvenSum.FindSumOfEvenNumbers(1, 2);
+            var evenSum = findEvenSum.FindSumOfEvenNumbers(1, 2);
 
-            //Console.WriteLine();
-            //Console.WriteLine($"Sum of multipes of 3 and 5 are: {multipleResult}");
-            //Console.WriteLine($"Count of the 10001st prime number: {primeCount}");
-            //Console.WriteLine($"Even sum of numbers less than 4 million in fibo: {evenSum}");
+            Console.WriteLine();
+            Console.WriteLine($"Sum of multipes of 3 and 5 are: {multipleResult}");
+            Console.WriteLine($"Count of the 10001st prime number: {primeCount}");
+            Console.WriteLine($"Even sum of numbers less than 4 million in fibo: {evenSum}");
 
             var sumOfEvenNumbers = (from number in Enumerable.Range(1, 4)
                                 group number by number % 2 into setOfNumbers
