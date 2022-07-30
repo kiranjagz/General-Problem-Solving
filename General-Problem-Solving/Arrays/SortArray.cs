f4aa8a182e0858c4045ac39acd1b4c06463c221a
Kiran <kiranjagz@gmai.com>
2022-07-30 21:14:25 +0200

    feat: added sorted array

---
+++ General-Problem-Solving/Arrays/SortArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General_Problem_Solving.Arrays
+{
+    internal class SortArray
+    {
+        public int[] LongSort(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        var temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
